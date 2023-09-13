15e15f227d5619137b35764ceb9cbc5adb6ffb2e
Ironboy <ironboy@users.noreply.github.com>
2023-09-13 13:30:50 +0200

    typo

--- auto-document/make-documentation.js
+++ auto-document/make-documentation.js
@@ -58,7 +58,7 @@ Object.defineProperty(globalThis, '_export', {
       exports.length ? `### Exports\n ${exports.map(x => '* ' + x).join('\n')}` : '',
       uses.length ? `\n\n### Uses\n ${uses.map(x => '*' + (x.exports.length ? x.exports.join(', ') + '* from' : '') + ' **' + x.file + '** ').map(x => ' * ' + x).join('\n')}` : '',
       usedBy.length ? `\n\n### Used by\n ${usedBy.map(x => '*' + x.exports.join(', ') + `* from [${x.file}](#${x.file.toLowerCase().replace(/\./g, '')})`).map(x => '* ' + x).join('\n')}` : '',
-      '\n### Code```js',
+      '\n### Code\n\n```js',
       `[theCodeHere]`,
       '```'
     ].join('\n'))
