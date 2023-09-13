abf8683b274d5cff86629d0c48ddfd43b1f979b2
Ironboy <ironboy@users.noreply.github.com>
2023-09-13 22:15:22 +0200

    comment change

--- make/_index.js
+++ make/_index.js
@@ -10,7 +10,7 @@
 /***
  * - Provides an export function for all JS files: _export
  * - Load all JS code in the make folder
- * - Calls make() to start the conversion process
+ * - Calls _make() to start the conversion process
  */
 
 import { readdirSync } from 'fs';
