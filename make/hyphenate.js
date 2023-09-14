9a814e619410da9c6eeee72f3d4c13bb1591fa7c
Ironboy <ironboy@users.noreply.github.com>
2023-09-14 04:50:45 +0200

    refactor hyphenation for less complexity

--- make/hyphenate.js
+++ make/hyphenate.js
@@ -3,14 +3,8 @@
  */
 
 export async function hyphenate(html) {
-  let {
-    hyphenateTags: els,
-    hyphenateMinWordLength: minWordLength,
-    hyphenateMinCharsBefore: minCharsBefore,
-    hyphenateMinCharsAfter: minCharsAfter,
-  } = settings;
   let { language } = settings;
-  // get correct hyphenator - fallback to 'en'
+  // get correct hyphenator dependning on language  - fallback to 'en'
   let hyphenator;
   while (!hyphenator) {
     hyphenator = await import('hyphen/' + language + '/index.js')
@@ -22,23 +16,8 @@ export async function hyphenate(html) {
       language = 'en';
     }
   }
+  // apply hyphenation
   hyphenator = hyphenator.default.hyphenateHTMLSync;
-  // hyphenate
-  for (let el of els) {
-    let reg = new RegExp(`<${el}[^>]*>.*?<\\/${el}>`, 'g');
-    html = html.replace(reg, x => {
-      let a = hyphenator(x, { minWordLength });
-      // adhere to minCharsBefore and minCharsAfter
-      let b = a.split('\u00AD');
-      let c = '';
-      for (let i = 0; i < b.length - 1; i++) {
-        let keep = !(b[i].slice(-minCharsBefore).replace(/[\p{L}-]/ug, ''))
-          && !(b[i + 1].slice(0, minCharsAfter).replace(/[\p{L}-]/ug, ''));
-        c += b[i] + (keep ? '\u00AD' : '');
-      }
-      c += b.slice(-1);
-      return c;
-    });
-  }
+  html = applyHyphenation(hyphenator, html);
   return { html, language };
 }
